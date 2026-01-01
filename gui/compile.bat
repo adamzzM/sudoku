37fedc24998afdfc27870f258daba41fd3abe625
Adamzz <cybergi10@googlemail.com>
2026-01-01 04:03:27 +0200

    added dll files cuz why not

--- gui/compile.bat
+++ gui/compile.bat
@@ -1,10 +1,6 @@
-set SFML_PATH=C:\SFML
+g++ -g main.cpp board.cpp popup.cpp ^
+ -IC:\SFML\include ^
+ -LC:\SFML\lib ^
+ -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system ^
+ -std=c++23 -o game.exe
 
-@REM g++ main.cpp ^
-@REM  -I%SFML_PATH%\include ^
-@REM  -L%SFML_PATH%\lib ^
-@REM  -lsfml-graphics -lsfml-window -lsfml-system ^
-@REM  -std=c++23 ^
-@REM  -o game.exe
-
- g++ -g main.cpp board.cpp popup.cpp -IC:\SFML\include -LC:\SFML\lib -lsfml-graphics -lsfml-window -lsfml-system -std=c++23 -o game.exe
